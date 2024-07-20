d7fa5f3c615f239665094eec1be522cf6b1802a1
Gaurav9571 <gauravmittal9571@gmail.com>
2024-07-20 13:46:09 +0530

    react-routing

---
+++ src/component/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>This is About page.</h1>
+        <br></br>
+        <h2>Hi,<br></br>My Name is Gaurav Mittal.</h2>
+        </div>
+  )
+}
+
+export default About
